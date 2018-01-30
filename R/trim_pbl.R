e10061de676065326e9ec793478bc9addf25b7bd
Lucas Henneman <henneman@hsph.harvard.edu>
2018-01-30 14:08:49 +0000

    use copy to trim

--- R/trim_pbl.R
+++ R/trim_pbl.R
@@ -14,12 +14,10 @@
 #' https://www.esrl.noaa.gov/psd/data/gridded/data.20thC_ReanV2.monolevel.mm.html )
 #' @return This function returns a trimmed dataset.
 
-trim_pbl <- function(M,
+trim_pbl <- function(Min,
                      rasterin){
   Sys.setenv(TZ='UTC')
-
-  #read in pbl file as raster brick
-#   rasterin <- rotate(brick(hpbl.nc, varname = 'hpbl' ))
+  M <- copy(Min)
 
   #get time vector to select layers
   dates <- names( rasterin)
