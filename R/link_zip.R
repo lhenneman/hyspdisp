5ef1f9b7b20774af1fc63cce5de7d0a20bc2eb77
Lucas Henneman <31038555+lhenneman@users.noreply.github.com>
2019-10-11 16:19:50 -0500

    Try to not test for intersection

--- R/link_zip.R
+++ R/link_zip.R
@@ -102,14 +102,14 @@ link_zip <- function( d,
   }
 
   # check if extent of inputdata covers zipcodes
-  e.zc <- extent( zc)
-  int <- intersect( e, e.zc)
-  print( e); print( extent( zc)); print( int)
-  if( length( int) == 0){
-    print('no ZIP overlap!')
-    out <- data.table( ZIP = character(), N = numeric())
-    return( out)
-  }
+  #e.zc <- extent( zc)
+  #int <- intersect( e, e.zc)
+  #print( e); print( extent( zc)); print( int)
+  #if( length( int) == 0){
+  #  print('no ZIP overlap!')
+  #  out <- data.table( ZIP = character(), N = numeric())
+  #  return( out)
+  #}
   
   #crop zip codes to only use ones over the extent
   zc_trim <- crop( zc, snap = 'out', e)
